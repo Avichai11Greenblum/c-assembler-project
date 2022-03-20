504e98ee2b65a490b25866863be6cb562e424525
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2022-03-20 21:17:30 +0200

    Update validation.h

--- validation.h
+++ validation.h
@@ -10,24 +10,27 @@
 #include "list.h"
 
 #define parse " \t\n"
-int whichDelivery(char myStr[], LIST *names);
-int isCurNumOfWords(char buffer[], int a);
-int isARegister(char []);
-int isACommand(char []);
-int isAValidName(char []);
-int isAGuidance(char []);
-int isACommentOrEmpty(char line []);
-int isAMacro(char []); /* not exist */
-int skip(char line[]); /* same as isAcommentOEempty*/
+#define parse_words "  \t\n"
+#define parse_delivery " ,\t\n["
+#define parse_commands " ,\t\n:"
+
+int validation(FILE *filePtr, LIST *names);
+int isRight(char line [] ,LIST *names);
+
 LIST *validNames(FILE *fileName, char *nameOfFile);
+int whichDelivery(char myStr[], LIST *names);
+
+int validOperands(char [], char [], char [], LIST*);
+
 void insertMa(char [], LIST*);
 void insertEx(char [], LIST*);
 void insertEn(char [], LIST*);
 void insertLD(char [], LIST*);
-int regTenToFifthTeen(char reg []);
-int checkForLabelAtBegining( LIST *names, char token[], int lineNumber);
+
 int checkForMacroAtSecond( LIST *names, char token[], int lineNumber );
 int checkForExternAtSecond( LIST *names, char token[], int lineNumber );
 int checkForEntryAtSecond( LIST *names, char token[], int lineNumber );
-int isValidString(char myStr []);
+int checkForLabelAtBegining( LIST *names, char token[], int lineNumber);
+
+
 #endif
