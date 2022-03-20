4fcc8c444deede19ea9ae0c1d5309b6597435c08
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2022-03-20 21:25:31 +0200

    Update toBinaryFirst.h

--- toBinaryFirst.h
+++ toBinaryFirst.h
@@ -1,67 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#ifndef _toBinary_H_
+#define _toBinary_H_
 
 #include "symbolTable.h"
+#include "validation.h"
 
-
-int IC = 100;
-int DC = 0;
-
-typedef struct commandsStruct{
-    char commandName[31];
-    int opcode;
-    int funct;
-    int numOfParam;     
-}commandsStruct;
-
-commandsStruct ourCommands[] = {
-    {"mov", 0, 0, 2},
-    {"cmp", 1, 0, 2},
-    {"add", 2, 10, 2},
-    {"sub", 2, 11, 2},
-    {"lea", 4, 0, 2},
-
-    /* second group -> have 1 operand */
-    {"clr", 5, 10, 1},
-    {"not", 5, 11, 1},
-    {"inc", 5, 12, 1},
-    {"dec", 5, 13, 1},
-    {"jmp", 9, 10, 1},
-    {"bne", 9, 11, 1},
-    {"jsr", 9, 12, 1},
-    {"red", 12, 0, 1},
-    {"prn", 13, 0, 1},
-
-    /* third group -> no operands */
-    {"rts", 14, 0, 0},
-    {"stop", 15, 0, 0}
-};
-
+/*an objest that holds the 20 bytes array for each word in line and a pointer to the next (implementation of linked list)*/
 typedef struct WORD
 {
     int word[20];
-    WORD *next;
+    struct WORD *next;
 }WORD;
+//extern struct WORD;
 
-typedef struct LINE
-{
-    WORD *wordHead;
-    LINE *next;
-}LINE;
-
-char *cutWhiteChars(char *str);
-
-LINE *firstPass(FILE *filePointer, symbolLink *headOfTable);
-LINE *toBinaryCommand(char line[], symbolLink *headOfTable);
-LINE *toBinaryGuidance(char line[]);
-WORD *deliveryForBinary(commandsStruct *command ,char myStr[], symbolLink *headOfTable);
-WORD *extraWordsToBinary(char *param);
+void output(WORD *head);
+WORD *firstPass(FILE *filePointer, symbolLink *headOfTable);
+void secondPass(FILE*, WORD*, symbolLink *, char *);
+void freeList1(WORD *head);
+void toBinaryCommand(char [], symbolLink *, WORD *);
+void toBinaryGuidance(char [], WORD *);
+void deliveryForBinary(commandsStruct * ,char [], symbolLink *, WORD *);
+void extraWordsToBinary(char *, WORD *, symbolLink *);
+void changeWord(WORD *, symbolLink *, FILE *, FILE *);
 WORD *charToBinary(char ch);
-void addLine(LINE *head, LINE *link);
 void addWord(WORD *head, WORD *link);
-int isNum(char *str);
 int extractRegister(char * param);
-int isARegister(char line []);
-int * decToBinary(int num);
+int *decToBinary(int num);
+void zeroMe(int word[]);
+int allZero(WORD *link);
+
+void binToSpecial (int num [],int address,FILE* output);
+char binToHex(int num []);
+
+
+#endif
